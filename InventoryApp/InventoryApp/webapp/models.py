5b59622529ab6c33a11a45f1210a23eb80322e46
Eric Barrera <Eric.Barrera@amd.com>
2017-07-03 17:02:17 -0500

    yo

--- InventoryApp/InventoryApp/webapp/models.py
+++ InventoryApp/InventoryApp/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    site = models.CharField(max_length=200)
+    platform = models.CharField(max_length=200)
+    revision = models.CharField(max_length=200)
+    bmc_hostname = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    use = models.CharField(max_length=200)
+    processors_number = models.CharField(max_length=200)
+    silicon_rev = models.CharField(max_length=200)
+    tdp = models.CharField(max_length=200)
+    mem_config = models.CharField(max_length=200)
+    pci = models.CharField(max_length=200)
+    field = models.CharField(max_length=200)
+    pxe = models.CharField(max_length=200)
+    dvd = models.CharField(max_length=200)
+    os = models.CharField(max_length=200)
+    wombat_ip = models.CharField(max_length=200)
+    rpi = models.CharField(max_length=200)
+    pdu_info = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    comments = models.CharField(max_length=200)
+    bmc_version = models.CharField(max_length=200)
+    bmc_ip = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.bmc_hostname
--- InventoryApp/InventoryApp/webapp/views.py
+++ InventoryApp/InventoryApp/webapp/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Board
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the InventoryApp index.")
+    boards_list = Board.objects.all()
+    template = loader.get_template('webapp/index.html')
+    context = {
+        'boards_list': boards_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def boards(request):
+    return HttpResponse("You're looking at board %s." % board_id)
