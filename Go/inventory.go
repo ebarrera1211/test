671261ee5083a71b7afa104b96153e48cbd16907
ebarrera1211 <ebarrera1211@gmail.com>
2017-12-20 23:35:19 -0600

    tutorial 1.2 complete

--- Go/inventory.go
+++ Go/inventory.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Go Web Dev")
+	})
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
