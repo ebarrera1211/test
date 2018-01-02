860d3db6dde990929c94502e8ad3eff637018baf
Eric Barrera <Eric.Barrera@amd.com>
2018-01-02 17:43:20 -0600

    finished 2.1

--- Go/inventory.go
+++ Go/inventory.go
@@ -8,11 +8,20 @@ import (
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
+
+	"encoding/json"
 )
 
 type Page struct {
 	Name     string
 	DBStatus bool
+}
+
+type SearchResult struct {
+	Title  string
+	Author string
+	Year   string
+	ID     string
 }
 
 func main() {
@@ -32,5 +41,19 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results := []SearchResult{
+			SearchResult{"Mody-Dick", "Herman Melville", "1851", "222222"},
+			SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "444444"},
+			SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
